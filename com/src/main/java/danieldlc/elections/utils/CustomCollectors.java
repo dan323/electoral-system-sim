f311abc92137797eda004135f9d0d2328e0ec72b
dan232 <danconsa@hotmail.com>
2018-12-04 22:38:05 +0100

    Towards CustomCollectors testing

--- com/src/main/java/danieldlc/elections/utils/CustomCollectors.java
+++ com/src/main/java/danieldlc/elections/utils/CustomCollectors.java
@@ -1,10 +1,8 @@
 package danieldlc.elections.utils;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collector;
 
@@ -15,9 +13,9 @@ public final class CustomCollectors {
 
     /**
      * Collects to a Map following the functions keyFunc and valueFunc. Make sure
-     * that keyFunc returns values not in the map already; otherwise the behaviour
-     * of this method is not assured. Beware that the map entering the Biconsumer
-     * is not the whole map if on a parallel stream.
+     * that keyFunc returns values not in the map already.
+     *
+     * Not recommended for parallel streams. It is safe, but costly.
      *
      * @param keyFunc
      * @param valueFunc
@@ -28,40 +26,7 @@ public final class CustomCollectors {
      */
     public static <S, R, T> Collector<S, Map<R, T>, Map<R, T>> toMap(BiFunction<S, Map<R, T>, R> keyFunc, BiFunction<S, Map<R, T>, T> valueFunc) {
 
-        return Collector.of(HashMap::new,
-                (Map<R, T> map, S s) -> map.put(keyFunc.apply(s, map), valueFunc.apply(s, map)),
-                CustomCollectors::mergeMaps,
-                Collector.Characteristics.IDENTITY_FINISH,
-                Collector.Characteristics.UNORDERED,
-                Collector.Characteristics.CONCURRENT);
-    }
-
-    /**
-     * The same as {@link CustomCollectors#toMap}, but with a custom merging function to be able to take care
-     * of concurrent or parallel execution problems.
-     *
-     * @param keyFunc
-     * @param valueFunc
-     * @param mergeMappings
-     * @param <S>
-     * @param <R>
-     * @param <T>
-     * @return
-     */
-    public static <S, R, T> Collector<S, Map<R, T>, Map<R, T>> toMap(BiFunction<S, Map<R, T>, R> keyFunc, BiFunction<S, Map<R, T>, T> valueFunc,
-                                                                     BinaryOperator<Map<R, T>> mergeMappings) {
-
-        return Collector.of(HashMap::new,
-                (Map<R, T> map, S s) -> map.put(keyFunc.apply(s, map), valueFunc.apply(s, map)),
-                mergeMappings,
-                Collector.Characteristics.IDENTITY_FINISH,
-                Collector.Characteristics.UNORDERED,
-                Collector.Characteristics.CONCURRENT);
-    }
-
-    private static <R, T> Map<R, T> mergeMaps(Map<R, T> map1, Map<R, T> map2) {
-        map1.putAll(map2);
-        return map1;
+        return new MapKeyValueCheckingCollector<>(keyFunc, valueFunc, false);
     }
 
     public static <S, R> Collector<S, Map<R, Integer>, Map<R, Integer>> toCountingMap(Function<S, R> function) {
