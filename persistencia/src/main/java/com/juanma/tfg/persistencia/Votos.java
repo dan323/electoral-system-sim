210ae7b514896cdd9029f3d1bedd7b89b489b4a5
jmcd0002 <jmcd0002@red.ujaen.es>
2019-02-04 18:23:45 +0100

    Introduccion JPA

---
+++ persistencia/src/main/java/com/juanma/tfg/persistencia/Votos.java
@@ -0,0 +1,50 @@
+package com.juanma.tfg.persistencia;
+
+import javax.persistence.*;
+import java.util.Map;
+
+@Entity
+public class Votos {
+    @ElementCollection
+    @JoinTable(name = "BATCH_VOTOS",
+            joinColumns = @JoinColumn(name = "ID"))
+    @MapKey(name = "PARTIDO")
+    @Column(name = "VOTOS")
+    private Map<String, Integer> votos;
+    private String nombre;
+    private String usuario;
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, Integer> getVotos() {
+        return votos;
+    }
+
+    public void setVotos(Map<String, Integer> votos) {
+        this.votos = votos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
