4358695b99e9166dfaf9fa5b95135bbea8b643bc
dan232 <danconsa@hotmail.com>
2019-08-10 17:29:31 +0200

    Update

--- com/elections/src/main/java/module-info.java
+++ com/elections/src/main/java/module-info.java
@@ -1,3 +1,7 @@
 module elections {
+    exports com.dan323.elections.systems.div;
+    exports com.dan323.elections.systems.quo;
+    exports com.dan323.elections.systems;
+    exports com.dan323.elections.simulations;
     requires utils;
 }
